5b7fb8acccfc02c489b94f046c5aa790d705748a
Floris Botermans <f.botermans@student.avans.nl>
2018-12-04 17:00:06 +0100

    cors test

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const routes = require('./routes/routes');
@@ -11,6 +12,8 @@ if (process.env.NODE_ENV !== 'test') {
     mongoose.connect('mongodb://admin:Secret123@ds151082.mlab.com:51082/game', { useNewUrlParser: true });
     console.log('connected');
 }
+
+app.use(cors());
 
 app.use(bodyParser.json());
 routes(app);
