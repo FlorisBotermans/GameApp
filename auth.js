f7e7781b1d806eedb50377ed569ce1bbefe9ec5f
Floris Botermans <f.botermans@student.avans.nl>
2018-12-05 23:03:28 +0100

    efeaf

--- auth.js
+++ auth.js
@@ -1,5 +1,5 @@
 const bcrypt = require('bcryptjs');
-const User = require('./models/user');
+const User = mongoose.model('user');
 
 exports.authenticate = (email, password) => {
     return new Promise(async (resolve, reject) => {
