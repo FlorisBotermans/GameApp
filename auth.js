d78333b6f689081a552de4b832eaa9c0f2d74f69
Floris Botermans <f.botermans@student.avans.nl>
2018-12-05 23:01:52 +0100

    errewqrqr

--- auth.js
+++ auth.js
@@ -2,23 +2,20 @@ const bcrypt = require('bcryptjs');
 const User = require('./models/user');
 
 exports.authenticate = (email, password) => {
-    return new Promise((resolve, reject) => {
+    return new Promise(async (resolve, reject) => {
         try {
             // Get user by email
-            User.findOne({ email })
-                .then((user) => {
-                    console.log(user);
-                    bcrypt.compare(password, user.password, (err, isMatch) => {
-                        if(err) throw err;
-                        if(isMatch) {
-                            resolve(user);
-                        } else {
-                            // Pass didn't match
-                            reject('Authentication failed');
-                        }
-                    });
-                })
-                .catch(reject('Authentication failed'));
+            const user = await User.findOne({ email });
+            
+            bcrypt.compare(password, user.password, (err, isMatch) => {
+                if(err) throw err;
+                if(isMatch) {
+                    resolve(user);
+                } else {
+                    // Pass didn't match
+                    reject('Authentication failed');
+                }
+            });
         } catch(err) {
             // Email not found
             reject('Authentication failed');
