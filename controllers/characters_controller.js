49cb6c1e04418f3e32dd1192a565862133af08a3
Floris Botermans <f.botermans@student.avans.nl>
2018-12-11 14:56:11 +0100

    ff

--- controllers/characters_controller.js
+++ controllers/characters_controller.js
@@ -18,7 +18,7 @@ module.exports = {
             { _id: req.params.gameid, "characters._id": req.params.characterid },
             { 
                 $set: {
-                    "characters.$": {
+                    characters: {
                         name: req.body.name,
                         title: req.body.title,
                         role: req.body.role
