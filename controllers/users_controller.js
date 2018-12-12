b5e412ededbd826d19cd3611b08ae04c927fca78
Floris Botermans <f.botermans@student.avans.nl>
2018-12-12 21:52:54 +0100

    ff

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -30,9 +30,5 @@ module.exports = {
                 }
             })
             .catch(() => res.status(501).send('Some internal error'));
-    },
-
-    getUsername(req, res, next) {
-        return res.status(200).json(decodedToken.userName);
     }
 }
