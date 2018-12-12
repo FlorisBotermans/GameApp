ef3e78926a8fe2781579e5541d1e05ef145df760
Floris Botermans <f.botermans@student.avans.nl>
2018-12-12 20:46:03 +0100

    rr

--- routes/routes.js
+++ routes/routes.js
@@ -7,7 +7,7 @@ module.exports = (app) => {
     // USER CRUD
     app.post('/api/register', UsersController.register);
     app.post('/api/login', UsersController.login);
-    app.get('/api/username', verifyToken, UsersController.getUsername);
+    app.get('/api/username', UsersController.getUsername, verifyToken);
 
     // GAME CRUD
     app.post('/api/games', GamesController.createGame);
