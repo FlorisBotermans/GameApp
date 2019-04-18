4230894ac3c642f4ed7e2b0305d7a267e4f50060
Floris Botermans <f.botermans@student.avans.nl>
2019-04-18 22:56:06 +0200

    bs

--- routes/routes.js
+++ routes/routes.js
@@ -9,24 +9,47 @@ module.exports = (app) => {
     app.post('/api/login', UsersController.login);
     app.post('/api/register', UsersController.register);
 
-    // GAME CRUD
-    app.post('/api/games', verifyToken, GamesController.createGame);
-    app.get('/api/games', verifyToken, GamesController.getGames);
-    app.get('/api/games/:gameid', verifyToken, GamesController.getGameById);
-    app.put('/api/games/:gameid', verifyToken, GamesController.editGame);
-    app.delete('/api/games/:gameid', verifyToken, GamesController.deleteGame);
-
-    // DEVELOPER CRUD
-    app.post('/api/games/:gameid/developers', verifyToken, DevelopersController.createDeveloper);
-    app.get('/api/games/:gameid/developers', verifyToken, DevelopersController.getDevelopers);
-    app.get('/api/games/:gameid/developers/:developerid', verifyToken, DevelopersController.getDeveloperById);
-    app.put('/api/games/:gameid/developers/:developerid', verifyToken, DevelopersController.editDeveloper);
-    app.delete('/api/games/:gameid/developers/:developerid', verifyToken, DevelopersController.deleteDeveloper);
-
-    // CHARACTER CRUD
-    app.post('/api/games/:gameid/characters', verifyToken, CharactersController.createCharacter);
-    app.get('/api/games/:gameid/characters', verifyToken, CharactersController.getCharacters);
-    app.get('/api/games/:gameid/characters/:characterid', verifyToken, CharactersController.getCharacterById);
-    app.put('/api/games/:gameid/characters/:characterid', verifyToken, CharactersController.editCharacter);
-    app.delete('/api/games/:gameid/characters/:characterid', verifyToken, CharactersController.deleteCharacter);
+    if (process.env.NODE_ENV !== 'test') {
+        // GAME CRUD
+        app.post('/api/games', verifyToken, GamesController.createGame);
+        app.get('/api/games', verifyToken, GamesController.getGames);
+        app.get('/api/games/:gameid', verifyToken, GamesController.getGameById);
+        app.put('/api/games/:gameid', verifyToken, GamesController.editGame);
+        app.delete('/api/games/:gameid', verifyToken, GamesController.deleteGame);
+    
+        // DEVELOPER CRUD
+        app.post('/api/games/:gameid/developers', verifyToken, DevelopersController.createDeveloper);
+        app.get('/api/games/:gameid/developers', verifyToken, DevelopersController.getDevelopers);
+        app.get('/api/games/:gameid/developers/:developerid', verifyToken, DevelopersController.getDeveloperById);
+        app.put('/api/games/:gameid/developers/:developerid', verifyToken, DevelopersController.editDeveloper);
+        app.delete('/api/games/:gameid/developers/:developerid', verifyToken, DevelopersController.deleteDeveloper);
+    
+        // CHARACTER CRUD
+        app.post('/api/games/:gameid/characters', verifyToken, CharactersController.createCharacter);
+        app.get('/api/games/:gameid/characters', verifyToken, CharactersController.getCharacters);
+        app.get('/api/games/:gameid/characters/:characterid', verifyToken, CharactersController.getCharacterById);
+        app.put('/api/games/:gameid/characters/:characterid', verifyToken, CharactersController.editCharacter);
+        app.delete('/api/games/:gameid/characters/:characterid', verifyToken, CharactersController.deleteCharacter);
+    } else {
+        // GAME CRUD
+        app.post('/api/games', GamesController.createGame);
+        app.get('/api/games', GamesController.getGames);
+        app.get('/api/games/:gameid', GamesController.getGameById);
+        app.put('/api/games/:gameid', GamesController.editGame);
+        app.delete('/api/games/:gameid', GamesController.deleteGame);
+    
+        // DEVELOPER CRUD
+        app.post('/api/games/:gameid/developers', DevelopersController.createDeveloper);
+        app.get('/api/games/:gameid/developers', DevelopersController.getDevelopers);
+        app.get('/api/games/:gameid/developers/:developerid', DevelopersController.getDeveloperById);
+        app.put('/api/games/:gameid/developers/:developerid', DevelopersController.editDeveloper);
+        app.delete('/api/games/:gameid/developers/:developerid', DevelopersController.deleteDeveloper);
+    
+        // CHARACTER CRUD
+        app.post('/api/games/:gameid/characters', CharactersController.createCharacter);
+        app.get('/api/games/:gameid/characters', CharactersController.getCharacters);
+        app.get('/api/games/:gameid/characters/:characterid', CharactersController.getCharacterById);
+        app.put('/api/games/:gameid/characters/:characterid', CharactersController.editCharacter);
+        app.delete('/api/games/:gameid/characters/:characterid', CharactersController.deleteCharacter);
+    }
 };
--- test/controllers/characters_controller_test.js
+++ test/controllers/characters_controller_test.js
@@ -5,7 +5,7 @@ const app = require('../../app');
 
 const Game = mongoose.model('game');
 
-describe('Developer controller', () => {
+describe('Character controller', () => {
     it('POST to api/games/gameid/characters creates a new character of a game', done => {
         const game = new Game({ name: 'testName1', description: 'testDescription1', platform: 'testPlatform1', category: 'testCategory1' });
 
@@ -13,8 +13,39 @@ describe('Developer controller', () => {
             request(app)
                 .post('/api/games/' + game._id + '/characters')
                 .send({ name: 'testName2', title: 'testTitle2', role: 'testRole2' })
+                .end(() => {
+                    Game.findOne({ name: 'testName1' })
+                        .then((game) => {
+                            assert(game.characters[0].title === 'testTitle2');
+                            done();
+                        });
+                });
+        });
+    });
+
+    it('GET to api/games/gameid/characters retrieves all characters of a game', done => {
+        const game = new Game({ name: 'testName1', description: 'testDescription1', platform: 'testPlatform1', category: 'testCategory1', characters: [{ name: 'testName2', title: 'testTitle2', role: 'testRole2' }, { name: 'testName3', title: 'testTitle3', role: 'testRole3' }] });
+
+        game.save().then(() => {
+            request(app)
+                .get('/api/games/' + game._id + '/characters')
                 .end((err, response) => {
-                    assert(response.body.name === 'testName2');
+                    assert(response.body.length === 2);
+                    assert(response.body[0].role === 'testRole2');
+                    assert(response.body[1].role === 'testRole3');
+                    done();
+                });
+        });
+    });
+
+    it('GET to api/games/gameid/characters/characterid retrieves a specific character of a game', done => {
+        const game = new Game({ name: 'testName1', description: 'testDescription1', platform: 'testPlatform1', category: 'testCategory1', characters: [{ name: 'testName2', title: 'testTitle2', role: 'testRole2' }, { name: 'testName3', title: 'testTitle3', role: 'testRole3' }] });
+
+        game.save().then(() => {
+            request(app)
+                .get('/api/games/' + game._id + '/characters/' + game.characters[1]._id)
+                .end((err, response) => {
+                    assert(response.body.name === 'testName3');
                     done();
                 });
         });
@@ -38,14 +69,17 @@ describe('Developer controller', () => {
     });
 
     it('DELETE to /api/games/gameid/characters/characterid deletes a character of a game', done => {
-        const game = new Game({ name: 'testName1', description: 'testDescription1', platform: 'testPlatform', category: 'testCategory', characters: [{ name: 'testName2', title: 'testTitle2', role: 'testRole2' }] });
+        const game = new Game({ name: 'testName1', description: 'testDescription1', platform: 'testPlatform', category: 'testCategory', characters: [{ name: 'testName2', title: 'testTitle2', role: 'testRole2' }, { name: 'testName3', title: 'testTitle3', role: 'testRole3' }] });
 
         game.save().then(() => {
             request(app)
                 .delete('/api/games/' + game._id + '/characters/' + game.characters[0]._id)
                 .end((err, response) => {
-                    assert(response.status === 204);
-                    done();
+                    Game.findOne({ name: 'testName1' })
+                        .then((game) => {
+                            assert(game.characters.length === 1);
+                            done();
+                        });
                 });
         });
     });
